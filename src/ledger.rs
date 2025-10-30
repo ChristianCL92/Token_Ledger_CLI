1e9ef3d850d81856e7d6929471481fbc6674085e
Christian <chrisdev92@outlook.com>
2025-10-30 11:10:05 +0100

    Defining Ledger struct and implementing account management methods

--- src/ledger.rs
+++ src/ledger.rs
@@ -1,6 +1,27 @@
 use std::collections::HashMap;
 
 pub struct Ledger {
-
+    accounts: HashMap<String, u64>
     
+}
+
+impl Ledger {
+    pub fn new() -> Self {
+        Self {
+            accounts: HashMap::new()
+        }
+    }
+
+    pub fn create_account (&mut self, name: String, initial_balance: u64) -> Result<(), String> {
+        if self.accounts.contains_key(&name) {
+            return Err(format!("Account '{}' already exists", name));
+        }
+
+        self.accounts.insert(name.clone(), initial_balance);
+        Ok(())
+    }
+
+    pub fn get_balance (&self, name: &str) -> Option<u64> {
+        self.accounts.get(name).copied()
+    }
 }
