2e5e8a14b17151162eb7ff9e2ea530ad431d4d67
Christian <chrisdev92@outlook.com>
2025-10-30 16:20:59 +0100

    implement transaction method and list method for listing in ledger

--- src/ledger.rs
+++ src/ledger.rs
@@ -24,4 +24,42 @@ impl Ledger {
     pub fn get_balance (&self, name: &str) -> Option<u64> {
         self.accounts.get(name).copied()
     }
+
+    pub fn transfer(&mut self, from: &str , to: &str,  amount: u64) -> Result<(), String> {
+        if !self.accounts.contains_key(from) {
+            return Err(format!("Could not find account {}", from))
+        }
+
+        if !self.accounts.contains_key(to) {
+            return Err(format!("receiving account does not exist{}", to))
+        }
+
+      let sender_balance= self.accounts.get(from).expect("found no sender balance");
+
+      if *sender_balance < amount {
+        return Err(format!(
+            "Insufficient funds. {} has {} tokens but tried to send {}",
+            from, sender_balance, amount
+        ));
+    }
+
+        *self.accounts.get_mut(from).unwrap() -= amount;
+        *self.accounts.get_mut(to).unwrap() += amount;
+
+    Ok(())
+    
+    }
+
+    pub fn list_accounts(&self){
+        if self.accounts.is_empty() {
+            println!("No accounts created yet, Create one with: create <name> <amount>");
+            return;
+        }
+
+        for (key, value) in &self.accounts {
+            println!("Registered accounts with name {} and amount {}", key, value);
+        }
+
+        println!();
+    }
 }
